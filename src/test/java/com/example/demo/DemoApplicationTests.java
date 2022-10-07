906f4b0d82c36fb779e6cbc8cabd30b794a06387
Anuj Dawar <an877696@dal.ca>
2022-10-07 00:59:09 -0300

    added failed test case

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +11,12 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 		System.out.println("Hello Test OK");
+	}
+	
+	@Test
+	void testFail()
+	{
+		fail("Failed");
 	}
 
 }
