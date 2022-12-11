81b4a0159451a3c25318926874a597ed9c46b668
Anuj Dawar <an877696@dal.ca>
2022-12-11 14:24:22 -0400

    Added Register Module
    Created  controller and registerUser.html

---
+++ src/main/java/dal/csci5308/project/group15/elearning/register/RegisterUserController.java
@@ -0,0 +1,11 @@
+package dal.csci5308.project.group15.elearning.register;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterUserController
+{
+    
+}
