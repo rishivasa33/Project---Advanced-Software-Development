fabfdbba2624b143c8aacf39b7fd149de08d54e6
Anuj Dawar <an877696@dal.ca>
2022-12-12 15:55:10 -0400

    Added Notification Feature & Added Login Controller to control the landing Page
    Show deadline notifications on student dashboard

---
+++ src/main/java/dal/csci5308/project/group15/elearning/deadlineNotification/CourseMaterialDeadlineNotification.java
@@ -0,0 +1,77 @@
+package dal.csci5308.project.group15.elearning.deadlineNotification;
+
+public class CourseMaterialDeadlineNotification
+{
+    private String subjectId;
+    private String materialId;
+    private String materialName;
+    private String startDate;
+    private String endDate;
+
+    private String numDays;
+
+    public CourseMaterialDeadlineNotification()
+    {
+
+    }
+
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(String subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getMaterialId() {
+        return materialId;
+    }
+
+    public void setMaterialId(String materialId) {
+        this.materialId = materialId;
+    }
+
+    public String getMaterialName() {
+        return materialName;
+    }
+
+    public void setMaterialName(String materialName) {
+        this.materialName = materialName;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getNumDays() {
+        return numDays;
+    }
+
+    public void setNumDays(String numDays) {
+        this.numDays = numDays;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseMaterialDeadlineNotification{" +
+                "subjectId='" + subjectId + '\'' +
+                ", materialId='" + materialId + '\'' +
+                ", materialName='" + materialName + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", numDays='" + numDays + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/dal/csci5308/project/group15/elearning/deadlineNotification/CourseMaterialDeadlineNotificationHandler.java
@@ -0,0 +1,63 @@
+package dal.csci5308.project.group15.elearning.deadlineNotification;
+
+import dal.csci5308.project.group15.elearning.database.DatabaseOperations;
+import dal.csci5308.project.group15.elearning.database.IDatabaseOperations;
+import dal.csci5308.project.group15.elearning.factory.forum.ForumFactory;
+import dal.csci5308.project.group15.elearning.factory.notification.CourseMaterialDeadlineNotificationFactory;
+import dal.csci5308.project.group15.elearning.factory.notification.ICourseMaterialDeadlineNotificationFactory;
+import dal.csci5308.project.group15.elearning.factory.properties.IPropertiesFactory;
+import dal.csci5308.project.group15.elearning.factory.properties.PropertiesFactory;
+import dal.csci5308.project.group15.elearning.models.forum.ForumTopic;
+import dal.csci5308.project.group15.elearning.models.forum.ForumTopicResponse;
+import dal.csci5308.project.group15.elearning.security.AuthUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class CourseMaterialDeadlineNotificationHandler implements ICourseMaterialDeadlineNotificationHandler
+{
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public CourseMaterialDeadlineNotificationHandler()
+    {
+
+    }
+
+    public List<CourseMaterialDeadlineNotification> getCourseMaterialDeadlineNotifications()
+    {
+        IDatabaseOperations databaseOperations = DatabaseOperations.instance();
+        IPropertiesFactory propertiesFactory = PropertiesFactory.instance();
+        List<CourseMaterialDeadlineNotification> notificationList = new LinkedList<>();
+
+        try
+        {
+            Map<String, List<Object>> resultSet = databaseOperations.read(
+                    propertiesFactory.makeSqlProperties().getPropertiesMap().get("STORED_PROCEDURE_DEADLINE_NOTIFICATION"), AuthUser.getUsername());
+
+            for (int row = 0; row < databaseOperations.getRowCount(resultSet); row++)
+            {
+                CourseMaterialDeadlineNotification notification = CourseMaterialDeadlineNotificationFactory.instance().makeCourseMaterialDeadlineNotification();
+
+                notification.setSubjectId(String.valueOf(databaseOperations.getValueAt(resultSet, "SUBJECT_ID", row)));
+                notification.setSubjectId(String.valueOf(databaseOperations.getValueAt(resultSet, "MATERIAL_ID", row)));
+                notification.setSubjectId(String.valueOf(databaseOperations.getValueAt(resultSet, "DESCRIPTION_TEXT", row)));
+                notification.setSubjectId(String.valueOf(databaseOperations.getValueAt(resultSet, "START_DATE", row)));
+                notification.setSubjectId(String.valueOf(databaseOperations.getValueAt(resultSet, "END_DATE", row)));
+                notification.setSubjectId(String.valueOf(databaseOperations.getValueAt(resultSet, "NUM_DAYS", row)));
+
+                notificationList.add(notification);
+            }
+        }
+        catch (SQLException sqlException)
+        {
+            logger.error(sqlException.getMessage());
+            logger.debug(sqlException.getStackTrace().toString());
+        }
+
+        return notificationList;
+    }
+}
