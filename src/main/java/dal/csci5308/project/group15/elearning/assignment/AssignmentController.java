d617cc8d49141e7777a14cefc7162bc6f2e3dc95
Utsav12198 <singh.utsav100@gmail.com>
2022-12-14 11:18:16 -0400

    refactored controller

--- src/main/java/dal/csci5308/project/group15/elearning/assignment/AssignmentController.java
+++ src/main/java/dal/csci5308/project/group15/elearning/assignment/AssignmentController.java
@@ -5,17 +5,15 @@ import dal.csci5308.project.group15.elearning.models.assignment.Assignment;
 import dal.csci5308.project.group15.elearning.models.assignment.IAssignmentFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
 @Controller
-@SessionAttributes({"assignmentId","student_number"})
+@SessionAttributes({"assignmentId", "student_number"})
 
 public class AssignmentController {
 
@@ -28,9 +26,6 @@ public class AssignmentController {
     @PostMapping("/saveAssignment")
     public String postAssignment(@ModelAttribute("assignment") AssignmentParams assignment, Model model) throws SQLException, IOException {
 
-       // String fileName = StringUtils.cleanPath(assignment.getFile().getOriginalFilename());
-
-        //Assignment assignment_model = new Assignment(assignment);
         IAssignmentFactory assignmentFactory = FactoryFacade.instance().getAssignmentFactory();
         Assignment assignment_model = assignmentFactory.createAssignment(assignment);
         try {
@@ -39,21 +34,17 @@ public class AssignmentController {
         } catch (SQLException exception) {
             return "assignmentDefault";
         }
-
     }
 
     @GetMapping("/loadAssignment/{courseInstanceID}")
     public ModelAndView loadAssignment(@PathVariable String courseInstanceID) {
 
-
         ModelAndView model = new ModelAndView("viewAssignment");
-        //Assignment assignment_model = new Assignment();
         IAssignmentFactory assignmentFactory = FactoryFacade.instance().getAssignmentFactory();
         Assignment assignment_model = assignmentFactory.createAssignment();
 
         try {
             List<String> assignmentList = assignment_model.loadAssignmentList(courseInstanceID);
-            System.out.println(assignmentList);
             model.addObject("assignmentList", assignmentList);
             return model;
         } catch (SQLException exception) {
@@ -64,17 +55,15 @@ public class AssignmentController {
     }
 
     @GetMapping("/loadAssignmentDetails/{assignmentId}")
-    public String loadAssignmentDetails(@PathVariable String assignmentId,@ModelAttribute("assignment") AssignmentParams assignment ,Model model) {
+    public String loadAssignmentDetails(@PathVariable String assignmentId, @ModelAttribute("assignment") AssignmentParams assignment, Model model) {
 
         model.addAttribute(assignment);
-        model.addAttribute("assignmentId",assignmentId);
-       // Assignment assignment_model = new Assignment();
+        model.addAttribute("assignmentId", assignmentId);
         IAssignmentFactory assignmentFactory = FactoryFacade.instance().getAssignmentFactory();
         Assignment assignment_model = assignmentFactory.createAssignment();
         try {
             List<Assignment> assignmentDetails = assignment_model.loadAssignmentDetails(assignmentId);
             model.addAttribute("assignmentDetails", assignmentDetails);
-            System.out.println("Size of array" + assignmentDetails.size());
             return "viewAssignmentDetails";
         } catch (SQLException exception) {
             exception.printStackTrace();
@@ -83,23 +72,17 @@ public class AssignmentController {
     }
 
     @PostMapping("/student/submitStudentAssignment")
-    public String submitStudentAssignment( @ModelAttribute("assignment") AssignmentParams assignment, Model model) throws SQLException {
+    public String submitStudentAssignment(@ModelAttribute("assignment") AssignmentParams assignment, Model model) throws SQLException {
 
         String assignment_id = String.valueOf(model.getAttribute("assignmentId"));
         String student_number = String.valueOf(model.getAttribute("student_number"));
-        System.out.println("Debug session attributes " + assignment_id + "  " +student_number);
 
         IAssignmentFactory assignmentFactory = FactoryFacade.instance().getAssignmentFactory();
         Assignment assignment_model = assignmentFactory.createAssignment(assignment);
-        //Assignment assignment_model = new Assignment(assignment);
         assignment_model.SaveStudentAssignment(assignment_id, student_number);
         return "submittedAssignmentDefault";
 
     }
-
-
-
-
 
 
 }
