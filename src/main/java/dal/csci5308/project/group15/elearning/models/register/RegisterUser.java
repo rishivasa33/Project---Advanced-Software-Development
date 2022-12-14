3c3f9178bba37c333dba628e9c22ffe7cbcc1584
Anuj Dawar <an877696@dal.ca>
2022-12-13 23:27:07 -0400

    Refactored Register User
    Removed handler and moved register user methods in RegisterUser class for SOLID

--- src/main/java/dal/csci5308/project/group15/elearning/models/register/RegisterUser.java
+++ src/main/java/dal/csci5308/project/group15/elearning/models/register/RegisterUser.java
@@ -1,4 +1,4 @@
-package dal.csci5308.project.group15.elearning.models.Register;
+package dal.csci5308.project.group15.elearning.models.register;
 
 import dal.csci5308.project.group15.elearning.database.IDatabaseOperations;
 import dal.csci5308.project.group15.elearning.factory.encoder.EncoderFactory;
@@ -14,14 +14,102 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class RegisterUserHandler implements IRegisterUserHandler {
+public class RegisterUser
+{
+    private String firstName;
+    private String lastName;
+    private String role;
+    private String email;
+    private String defaultPassword;
+    private String program;
+    private Map<String, String> programMap;
+
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public RegisterUserHandler() {
+    public RegisterUser() {
+
+    }
+
+    public RegisterUser(String firstName, String lastName, String userType, String email, String defaultPassword, String program, Map<String, String> programMap) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = userType;
+        this.email = email;
+        this.defaultPassword = defaultPassword;
+        this.program = program;
+        this.programMap = programMap;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDefaultPassword() {
+        return defaultPassword;
+    }
+
+    public void setDefaultPassword(String defaultPassword) {
+        this.defaultPassword = defaultPassword;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public Map<String, String> getProgramMap() {
+        return programMap;
+    }
+
+    public void setProgramMap(Map<String, String> programMap) {
+        this.programMap = programMap;
     }
 
     @Override
-    public int createNewUser(IDatabaseOperations databaseOperations, User user)
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", role='" + role + '\'' +
+                ", email='" + email + '\'' +
+                ", defaultPassword='" + defaultPassword + '\'' +
+                ", program='" + program + '\'' +
+                ", programMap=" + programMap +
+                '}';
+    }
+
+    public int createNewUser(IDatabaseOperations databaseOperations, RegisterUser user)
     {
         IPropertiesFactory propertiesFactory = PropertiesFactory.instance();
         IEncoder encoder = EncoderFactory.instance().makeEncoder();
@@ -44,7 +132,6 @@ public class RegisterUserHandler implements IRegisterUserHandler {
         }
     }
 
-    @Override
     public Map<String, String> getAllProgramList(IDatabaseOperations databaseOperations) {
         Map<String, String> programMap = new HashMap<>();
         IPropertiesFactory propertiesFactory = PropertiesFactory.instance();
