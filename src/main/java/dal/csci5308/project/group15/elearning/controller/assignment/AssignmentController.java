fe76fd7d9abe3fb24569bd1ca08067ba8e5196f6
Utsav12198 <singh.utsav100@gmail.com>
2022-12-14 17:34:15 -0400

    refactored

--- src/main/java/dal/csci5308/project/group15/elearning/controller/assignment/AssignmentController.java
+++ src/main/java/dal/csci5308/project/group15/elearning/controller/assignment/AssignmentController.java
@@ -10,9 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
